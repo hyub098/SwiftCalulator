a3af9e3403b5fc2ae8f8800a44ff6b7086faf92d
Peter <hyub098@aucklanduni.ac.nz>
2017-04-18 22:04:12 +1200

    Taking input numbers and store it

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var numberOnScreen:Double = 0;
+    
+    @IBOutlet weak var outputLabel: UILabel!
 
+    @IBAction func numbers(_ sender: UIButton) {
+        outputLabel.text = outputLabel.text! + String(sender.tag-1);
+        numberOnScreen = Double(outputLabel.text!)!;
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
